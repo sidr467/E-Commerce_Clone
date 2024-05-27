d998067463b89d254ceb6703d82515e3ab26770e
sidr467 <146876430+sidr467@users.noreply.github.com>
2024-05-27 17:28:47 +0530

    Starting with Navbar for Mobile

--- src/app/components/Navbar.tsx
+++ src/app/components/Navbar.tsx
@@ -1,9 +1,12 @@
+import Link from "next/link"
+
 const Navbar = () => {
-    return (
-        <div>
-            Navbar
-        </div>
-    );
+  return (
+    <div className="h-20 px-4 md:px-8 lg:px-16 xl:px-32 2xl:px-64 ">
+      {/*Mobile */}
+      <Link href="/">SIDD</Link>
+    </div>
+  )
 }
 
-export default Navbar;
+export default Navbar
