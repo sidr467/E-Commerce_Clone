b9d58e13c304b9bacb7c93fb853ebd79be29b2da
sidr467 <146876430+sidr467@users.noreply.github.com>
2024-05-27 17:43:01 +0530

    Added Menu.tsx component
    in that added navigation links and for mobile hamburger menu icon

--- src/app/components/Navbar.tsx
+++ src/app/components/Navbar.tsx
@@ -1,10 +1,12 @@
 import Link from "next/link"
+import Menu from "./Menu"
 
 const Navbar = () => {
   return (
     <div className="h-20 px-4 md:px-8 lg:px-16 xl:px-32 2xl:px-64 ">
       {/*Mobile */}
       <Link href="/">SIDD</Link>
+      <Menu />
     </div>
   )
 }
