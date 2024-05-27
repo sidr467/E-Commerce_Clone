75738afd96167eecb1c4d3af87cfe69abb478a7e
sidr467 <146876430+sidr467@users.noreply.github.com>
2024-05-27 23:16:47 +0530

    Update SearchBar.tsx

--- src/app/components/SearchBar.tsx
+++ src/app/components/SearchBar.tsx
@@ -8,7 +8,11 @@ function SearchBar() {
       className=" flex items-center justify-center gap-4 bg-gray-100 p-2 rounded-md flex-1 "
       action=""
     >
-      <input type="text" placeholder="Search" className=" flex-1 bg-transparent outline-none" />
+      <input
+        type="text"
+        placeholder="Search"
+        className=" flex-1 bg-transparent outline-none"
+      />
       <button className=" cursor-pointer">
         <Image src="/search.png" alt="Search icon" width={16} height={16} />
       </button>
