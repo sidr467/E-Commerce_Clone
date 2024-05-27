30e6ca4ee124a0015c0930ff5c4411923e4c0581
sidr467 <146876430+sidr467@users.noreply.github.com>
2024-05-27 18:04:43 +0530

    Added some styles to menu links

--- src/app/components/Navbar.tsx
+++ src/app/components/Navbar.tsx
@@ -3,10 +3,14 @@ import Menu from "./Menu"
 
 const Navbar = () => {
   return (
-    <div className="h-20 px-4 md:px-8 lg:px-16 xl:px-32 2xl:px-64 ">
-      {/*Mobile */}
-      <Link href="/">SIDD</Link>
-      <Menu />
+    <div className="h-20 px-4 md:px-8 lg:px-16 xl:px-32 2xl:px-64 relative">
+      <div className="flex items-center justify-between h-full ">
+        {/*Mobile */}
+        <Link href="/">
+          <div className="text-2xl tracking-wide">SIDD</div>
+        </Link>
+        <Menu />
+      </div>
     </div>
   )
 }
