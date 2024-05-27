7184c9d2099104848c28068788b370f46bffa886
sidr467 <146876430+sidr467@users.noreply.github.com>
2024-05-27 23:09:23 +0530

    Added searchbar and style

--- src/app/components/Navbar.tsx
+++ src/app/components/Navbar.tsx
@@ -8,7 +8,7 @@ const Navbar = () => {
   return (
     <div className="h-20 px-4 md:px-8 lg:px-16 xl:px-32 2xl:px-64 relative">
       {/*Mobile */}
-      <div className="flex items-center justify-between h-full ">
+      <div className="flex items-center justify-between h-full md:hidden">
         <Link href="/">
           <div className="text-2xl tracking-wide">SIDD</div>
         </Link>
@@ -18,13 +18,13 @@ const Navbar = () => {
       <div className=" hidden md:flex items-center justify-between gap-8 h-full">
         {/* LEFT */}
         <div className=" w-1/3">
-          <Link href="/">
+          <Link href="/" className=" flex items-center gap-3">
             <Image src="/logo.png" alt="Logo Image" width={24} height={24} />
             <div className="text-2xl tracking-wide">SIDD</div>
           </Link>
         </div>
         {/* RIGHT */}
-        <div className=" w-2/3">
+        <div className=" w-2/3 flex items-center justify-between gap-8">
           <SearchBar />
           <NavIcons />
         </div>
--- src/app/components/SearchBar.tsx
+++ src/app/components/SearchBar.tsx
@@ -1,11 +1,19 @@
-'use client'
+"use client"
+
+import Image from "next/image"
 
 function SearchBar() {
-    return (
-        <div>
-            Enter
-        </div>
-    );
+  return (
+    <form
+      className=" flex items-center justify-center gap-4 bg-gray-100 p-2 rounded-md flex-1 "
+      action=""
+    >
+      <input type="text" placeholder="Search" className=" flex-1 bg-transparent outline-none" />
+      <button className=" cursor-pointer">
+        <Image src="/search.png" alt="Search icon" width={16} height={16} />
+      </button>
+    </form>
+  )
 }
 
-export default SearchBar;
+export default SearchBar
