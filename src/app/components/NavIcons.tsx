88c5f8106230ca1b2e712d5c58aa10d2ca3193da
sidr467 <146876430+sidr467@users.noreply.github.com>
2024-05-28 18:23:46 +0530

    Update NavIcons.tsx

--- src/app/components/NavIcons.tsx
+++ src/app/components/NavIcons.tsx
@@ -5,14 +5,27 @@ import Image from "next/image"
 function NavIcons() {
   return (
     <div className="flex gap-4 ic xl:gap-6 ">
-      <Image src="/profile.png" alt="Profile icon" width={22} height={22} />
+      <Image
+        src="/profile.png"
+        alt="Profile icon"
+        width={22}
+        height={22}
+        className=" cursor-pointer"
+      />
       <Image
         src="/notification.png"
         alt="Notification icon"
         width={22}
         height={22}
+        className=" cursor-pointer"
       />
-      <Image src="/cart.png" alt="Cart icon" width={22} height={22} />
+      <Image
+        src="/cart.png"
+        alt="Cart icon"
+        width={22}
+        height={22}
+        className=" cursor-pointer"
+      />
     </div>
   )
 }
