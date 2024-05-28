39d21a7c896f071f5e3167f89bdb4afabb9b460d
sidr467 <146876430+sidr467@users.noreply.github.com>
2024-05-28 19:35:05 +0530

    Update CartModal.tsx
    Added image and some styles to cart modal

--- src/app/components/CartModal.tsx
+++ src/app/components/CartModal.tsx
@@ -1,11 +1,42 @@
 "use client"
 
+import Image from "next/image"
+
 function CartModal() {
-  const cartItems = false
+  const cartItems = true
 
   return (
-    <div className=" absolute p-4 rounded-md bg-black text-white text-sm right-0 top-10 flex flex-col gap-6 z-20">
-      {!cartItems ? <div>Cart is empty</div> : <div>Cart</div>}
+    <div className="w-max absolute p-4 rounded-md bg-black text-white text-sm -right-2 top-10 flex flex-col gap-6 z-20">
+      {!cartItems ? (
+        <div>Cart is empty</div>
+      ) : (
+        <div className="flex gap-4">
+          <Image
+            src="/toy.jpeg"
+            alt="item"
+            width={72}
+            height={96}
+            className=" object-cover rounded-md"
+          />
+          <div className="flex justify-center flex-col w-full gap-8">
+            {/* Top */}
+            <div className="">
+              {/* Title */}
+              <div className="flex items-center justify-between gap-8">
+                <h3 className=" font-semibold">Product Name</h3>
+                <div className="p-1 bg-white text-black rounded-md">49$</div>
+              </div>
+              {/* Decription */}
+              <div className=" text-sm text-gray-500">Available</div>
+            </div>
+            {/* Bottom */}
+            <div className=" flex justify-between text-sm">
+              <span className=" text-gray-500">Qty. 2</span>
+              <span className=" text-blue-500">Remove</span>
+            </div>
+          </div>
+        </div>
+      )}
     </div>
   )
 }
