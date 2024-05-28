728e510d401712be4379baff73fd57eaa8380860
sidr467 <146876430+sidr467@users.noreply.github.com>
2024-05-28 18:44:04 +0530

    Added a state to profile icon
    Added a state to profile icon and for open it will show link of Profile and logout

--- src/app/components/NavIcons.tsx
+++ src/app/components/NavIcons.tsx
@@ -1,17 +1,42 @@
 "use client"
 
 import Image from "next/image"
+import { useState } from "react"
+import Link from "next/link"
+import { useRouter } from "next/navigation"
 
 function NavIcons() {
+  const [isProfileOpen, setIsProfileOpen] = useState(false)
+  const [isCartOpen, setIsCartOpen] = useState(false)
+
+  const router = useRouter()
+
+  //temporary
+  const isLoggedIn = false
+
+  function handleProfile() {
+    if (!isLoggedIn) {
+      router.push("/login")
+    }
+    setIsProfileOpen((prev) => !prev)
+  }
+
   return (
-    <div className="flex gap-4 ic xl:gap-6 ">
+    <div className="flex gap-4 ic xl:gap-6 relative ">
       <Image
         src="/profile.png"
         alt="Profile icon"
         width={22}
         height={22}
         className=" cursor-pointer"
+        onClick={handleProfile}
       />
+      {isProfileOpen && (
+        <div className=" absolute p-4 rounded-md text-sm -left-7 top-10 z-20 bg-black text-white">
+          <Link href="/">Profile</Link>
+          <div className="mt-2 cursor-pointer">Logout</div>
+        </div>
+      )}
       <Image
         src="/notification.png"
         alt="Notification icon"
@@ -25,7 +50,13 @@ function NavIcons() {
         width={22}
         height={22}
         className=" cursor-pointer"
+        onClick={() => setIsCartOpen((prev) => !prev)}
       />
+      {isCartOpen && 
+      <div className="">
+        
+      </div>
+      }
     </div>
   )
 }
