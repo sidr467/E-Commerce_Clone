11e5ca3f8ee8627ba61e1256d5465fc65f0ea53d
sidr467 <146876430+sidr467@users.noreply.github.com>
2024-05-28 19:10:29 +0530

    Update CartModal.tsx
    Creating CartModal which shows CartItems when we add and if Cart is empty it shows empty using conditional operation

--- src/app/components/CartModal.tsx
+++ src/app/components/CartModal.tsx
@@ -1,11 +1,13 @@
-'use client'
+"use client"
 
 function CartModal() {
-    return (
-        <div>
-            Enter
-        </div>
-    );
+  const cartItems = false
+
+  return (
+    <div className=" absolute p-4 rounded-md bg-black text-white text-sm right-0 top-10 flex flex-col gap-6 z-20">
+      {!cartItems ? <div>Cart is empty</div> : <div>Cart</div>}
+    </div>
+  )
 }
 
-export default CartModal;
+export default CartModal
