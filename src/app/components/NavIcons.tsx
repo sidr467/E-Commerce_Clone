9c35be15758a6e48d01e6ae2cd1ea67a941b5b97
sidr467 <146876430+sidr467@users.noreply.github.com>
2024-05-28 15:53:29 +0530

    Update NavIcons.tsx
    Added NavIcons and their CSS

--- src/app/components/NavIcons.tsx
+++ src/app/components/NavIcons.tsx
@@ -1,11 +1,20 @@
-'use client'
+"use client"
+
+import Image from "next/image"
 
 function NavIcons() {
-    return (
-        <div>
-            Enter
-        </div>
-    );
+  return (
+    <div className="flex gap-4 ic xl:gap-6 ">
+      <Image src="/profile.png" alt="Profile icon" width={22} height={22} />
+      <Image
+        src="/notification.png"
+        alt="Notification icon"
+        width={22}
+        height={22}
+      />
+      <Image src="/cart.png" alt="Cart icon" width={22} height={22} />
+    </div>
+  )
 }
 
-export default NavIcons;
+export default NavIcons
