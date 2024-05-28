c7974743710a5d2cdf8f6ca3c5e98ed95819500c
sidr467 <146876430+sidr467@users.noreply.github.com>
2024-05-28 15:33:16 +0530

    Update SearchBar.tsx
    Added handleSearch function to get name from input element and on form onSubmit which will search in from list
    page

--- src/app/components/SearchBar.tsx
+++ src/app/components/SearchBar.tsx
@@ -1,15 +1,31 @@
 "use client"
 
 import Image from "next/image"
+import { useRouter } from "next/navigation"
+import React from "react"
 
 function SearchBar() {
+  const router = useRouter()
+
+  function handleSearch(e: React.FormEvent<HTMLFormElement>) {
+    e.preventDefault()
+    const formData = new FormData(e.currentTarget)
+    const name = formData.get("name") as string
+
+    if (name) {
+      router.push(`/list?name=${name}`)
+    }
+  }
+
   return (
     <form
       className=" flex items-center justify-center gap-4 bg-gray-100 p-2 rounded-md flex-1 "
       action=""
+      onSubmit={handleSearch}
     >
       <input
         type="text"
+        name="name"
         placeholder="Search"
         className=" flex-1 bg-transparent outline-none"
       />
