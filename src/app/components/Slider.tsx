04a5780f4d6bdde7f0f505048a907ad2d951b30d
sidr467 <146876430+sidr467@users.noreply.github.com>
2024-05-29 18:47:23 +0530

    Style of slider is done

--- src/app/components/Slider.tsx
+++ src/app/components/Slider.tsx
@@ -2,6 +2,7 @@
 
 import { useState } from "react"
 import Link from "next/link"
+import Image from "next/image"
 
 const slides = [
   {
@@ -34,20 +35,37 @@ function Slider() {
   const [current, setCurrent] = useState(0)
 
   return (
-    <div className=" h[calc(100vh-80px)] overflow-hidden">
+    <div className=" h-[calc(100vh-80px)] overflow-hidden">
       <div className=" w-max h-full flex transition-all ease-in-out duration-1000">
         {slides.map((slide) => (
-          <div className="" key={slide.id}>
+          <div
+            className={`${slide.bg} w-screen h-full flex flex-col gap-16 xl:flex-row`}
+            key={slide.id}
+          >
             {/* Text container */}
-            <div className="">
-              <h2>{slide.description}</h2>
-              <h1>{slide.title}</h1>
+            <div className=" h-1/2 xl:w-1/2 xl:h-full flex flex-col items-center justify-center gap-8 2xl:gap-16 text-center">
+              <h2 className="text-xl lg:text-3xl 2xl:text-5xl">
+                {slide.description}
+              </h2>
+              <h1 className=" text-5xl lg:text-6xl 2xl:text-8xl font-semibold">
+                {slide.title}
+              </h1>
               <Link href={slide.url}>
-                <button>SHOP NOW</button>
+                <button className=" rounded-md py-3 px-4 bg-black text-white">
+                  SHOP NOW
+                </button>
               </Link>
             </div>
             {/* Image container */}
-            <div></div>
+            <div className=" h-1/2 xl:w-1/2 xl:h-full relative">
+              <Image
+                src={slide.img}
+                alt="sliderImage"
+                fill
+                sizes="100%"
+                className="object-cover"
+              />
+            </div>
           </div>
         ))}
       </div>
