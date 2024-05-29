e347cd44fde748a292664cc8c8ca548f05aa1cb2
sidr467 <146876430+sidr467@users.noreply.github.com>
2024-05-30 00:08:05 +0530

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -10,8 +10,8 @@ const HomePage = () => {
         <h1 className="text-2xl font-bold">Feature Products</h1>
         <ProductList />
       </div>
-      <div className="mt-24 ">
-        <h1 className="text-2xl font-bold">Category Products</h1>
+      <div className="mt-24  ">
+        <h1 className="text-2xl font-bold px-4 md:px-8 lg:px-16 xl:px-32 2xl:px-64 mb-12">Category Products</h1>
         <CategoryList />
       </div>
       <div className="mt-24 px-4 md:px-8 lg:px-16 xl:px-32 2xl:px-64">
