3c0e8f17a8e151f4bd7ce3ce94b9a50d646afaa8
sidr467 <146876430+sidr467@users.noreply.github.com>
2024-05-29 19:01:05 +0530

    Update Slider.tsx
    Added slider rings(buttons)

--- src/app/components/Slider.tsx
+++ src/app/components/Slider.tsx
@@ -69,6 +69,22 @@ function Slider() {
           </div>
         ))}
       </div>
+      <div>
+        <div className=" absolute m-auto left-1/2 bottom-8 flex gap-4">
+          {slides.map((slide, index) => (
+            <div
+              className={`w-3 h-3 rounded-full ring-1 ring-gray-600 cursor-pointer flex items-center justify-center ${
+                current === index ? "scale-150 " : ""
+              }`}
+              key={slide.id}
+            >
+              {current === index && (
+                <div className="w-[6px] h-[6px] bg-gray-600 rounded-full"></div>
+              )}
+            </div>
+          ))}
+        </div>
+      </div>
     </div>
   )
 }
