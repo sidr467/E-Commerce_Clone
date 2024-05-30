3a2e16a423a3733aa533043da08712a1b7775fc3
sidr467 <146876430+sidr467@users.noreply.github.com>
2024-05-30 15:37:48 +0530

    Added some Product Images and used Map

--- src/app/[slug]/page.tsx
+++ src/app/[slug]/page.tsx
@@ -2,13 +2,13 @@ import ProductImages from "../components/ProductImages"
 
 const SinglePage = () => {
   return (
-    <div>
+    <div className="px-4 md:px-8 lg:px-16 xl:px-32 2xl:px-64 relative flex flex-col lg:flex-row gap-16">
       {/* Image */}
-      <div className="">
+      <div className=" w-full lg:w-1/2 lg:sticky top-20 h-max">
         <ProductImages />
       </div>
       {/* Text */}
-      <div className=""></div>
+      <div className=" w-full lg:w-1/2 flex flex-col gap-6">TEXT</div>
     </div>
   )
 }
--- src/app/components/ProductImages.tsx
+++ src/app/components/ProductImages.tsx
@@ -1,9 +1,61 @@
+"use client"
+
+import { url } from "inspector"
+import Image from "next/image"
+import { useState } from "react"
+
+const images = [
+  {
+    id: 1,
+    url: "https://images.pexels.com/photos/4314063/pexels-photo-4314063.jpeg?auto=compress&cs=tinysrgb&w=600",
+  },
+  {
+    id: 2,
+    url: "https://images.pexels.com/photos/6654403/pexels-photo-6654403.jpeg?auto=compress&cs=tinysrgb&w=600",
+  },
+  {
+    id: 3,
+    url: "https://images.pexels.com/photos/18353382/pexels-photo-18353382/free-photo-of-black-gopro-camera.jpeg?auto=compress&cs=tinysrgb&w=600",
+  },
+  {
+    id: 4,
+    url: "https://images.pexels.com/photos/14722844/pexels-photo-14722844.jpeg?auto=compress&cs=tinysrgb&w=600",
+  },
+]
+
 function ProductImages() {
-    return (
-        <div>
-            Enter
-        </div>
-    );
+  const [index, setIndex] = useState(0)
+
+  return (
+    <div>
+      <div className="h-[500px] relative">
+        <Image
+          src={images[index].url}
+          alt=""
+          fill
+          sizes="50vw"
+          className="object-cover rounded-md"
+        />
+      </div>
+      <div className="">
+        {images.map((image, index) => (
+          <div
+            className=" w-1/4 h-32 relative gap-4 mt-8"
+            key={image.id}
+            onClick={() => setIndex(index )}
+          >
+            <Image
+              src={image.url}
+              alt=""
+              fill
+              sizes="30vw"
+              className="object-cover rounded-md"
+            />
+          </div>
+        ))}
+      </div>
+    </div>
+  )
 }
 
-export default ProductImages;
+export default ProductImages
