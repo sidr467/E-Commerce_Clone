0f2b6124757c6939313d99c8d1447e8be232b2a6
sidr467 <146876430+sidr467@users.noreply.github.com>
2024-05-30 10:33:43 +0530

    Finished Styling left side of Footer

--- src/app/components/Footer.tsx
+++ src/app/components/Footer.tsx
@@ -3,11 +3,11 @@ import Image from "next/image"
 
 const Footer = () => {
   return (
-    <div>
+    <div className=" py-24 px-4 md:px-8 lg:px-16 xl:px-32 2xl:px-64 bg-gray-100 text-sm mt-24">
       {/* Top */}
-      <div className="">
+      <div className=" flex justify-between gap-24">
         {/* Left */}
-        <div className=" w-1/4">
+        <div className=" w-full md:w-1/2 lg:w-1/4 flex flex-col gap-8">
           <Link href="/">
             <div className="text-2xl tracking-wide">SIDD</div>
           </Link>
@@ -15,8 +15,8 @@ const Footer = () => {
             Lorem ipsum dolor sit, amet consectetur adipisicing elit. A saepe
             eaque dignissimos commodi nam facere.
           </p>
-          <span>hello@sid123</span>
-          <span>+123456789</span>
+          <span className="font-semibold">hello@sid123</span>
+          <span className="font-semibold">+123456789</span>
           <div className="flex gap-6">
             <Image src="/facebook.png" alt="" width={16} height={16} />
             <Image src="/instagram.png" alt="" width={16} height={16} />
@@ -26,9 +26,19 @@ const Footer = () => {
           </div>
         </div>
         {/* Center */}
-        <div className="w-1/2"></div>
+        <div className="w-1/2 hidden lg:flex justify-between"></div>
         {/* Right */}
-        <div className="w-1/4"></div>
+        <div className=" w-full md:w-1/2 lg:w-1/4 flex flex-col gap-8">
+          <h1 className=" font-semibold text-lg">SUBSCRIBE</h1>
+          <p>Lorem ipsum dolor sit, amet consectetur adipisicing elite.</p>
+          <div className="flex">
+            <input
+              type="text"
+              placeholder="Email Address"
+              className=" p-4 w-3/4"
+            />
+          </div>
+        </div>
       </div>
       {/* Bottom */}
       <div className=""></div>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -11,11 +11,14 @@ const HomePage = () => {
         <ProductList />
       </div>
       <div className="mt-24  ">
-        <h1 className="text-2xl font-bold px-4 md:px-8 lg:px-16 xl:px-32 2xl:px-64 mb-12">Category Products</h1>
+        <h1 className="text-2xl font-bold px-4 md:px-8 lg:px-16 xl:px-32 2xl:px-64 mb-12">
+          Category Products
+        </h1>
         <CategoryList />
       </div>
       <div className="mt-24 px-4 md:px-8 lg:px-16 xl:px-32 2xl:px-64">
         <h1 className="text-2xl font-bold">Products</h1>
+        <ProductList />
       </div>
     </div>
   )
